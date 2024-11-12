4eb008f84002f6f16dcf2e926c5383dec7556d8e
Pradeep-Prady <pradeepprady005@gmail.com>
2024-11-12 16:30:15 +0530

    host  changed 10

--- index.js
+++ index.js
@@ -13,29 +13,69 @@ const app = express();
 
 dotenv.config();
 app.use(express.json());
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "http://localhost:3000/",
+//       "http://localhost:3000/",
+
+//       "http://localhost:3001",
+//       "https://eltribo-ruby.vercel.app",
+//       "https://eltribo-ruby.vercel.app/",
+//       "https://eltribo-ui.vercel.app/",
+//       "https://eltribo-ui.vercel.app"
+//       ,
+//     ],
+//     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = [
+  "https://vjpenterprises.in",
+  "http://vjpenterprises.in",
+  "https://www.vjpenterprises.in",
+  "http://www.vjpenterprises.in",
+  "http://127.0.0.1:5173",
+  "http://localhost:5173",
+  "http://localhost:7000",
+  "http://localhost:5173/",
+  "http://localhost:5174/",
+  "http://127.0.0.1:7000",
+  "https://vjp.vercel.app",
+  "https://vjp.vercel.app/",
+
+  "http://localhost:3000",
+  "http://localhost:3000/",
+  "http://localhost:3000/",
+
+  "http://localhost:3001",
+  "https://eltribo-ruby.vercel.app",
+  "https://eltribo-ruby.vercel.app/",
+  "https://eltribo-ui.vercel.app/",
+  "https://eltribo-ui.vercel.app",
+];
+app.use(cookieParser());
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3000/",
-      "http://localhost:3000/",
-
-      "http://localhost:3001",
-      "https://eltribo-ruby.vercel.app",
-      "https://eltribo-ruby.vercel.app/",
-      "https://eltribo-ui.vercel.app/",
-      "https://eltribo-ui.vercel.app"
-      ,
-    ],
-    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
+
 app.use(morgan("combined"));
 app.use(passport.initialize());
 
 app.use("/api", indexRoute);
-
 
 app.use(globalResponseController);
 
@@ -46,5 +86,4 @@ const server = app.listen(process.env.PORT, () => {
 process.on("SIGTERM", () => shutdown(server));
 process.on("SIGINT", () => shutdown(server));
 
-
-console.log('inside ************************************************')
+console.log("inside ************************************************");
